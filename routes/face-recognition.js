165c091e8e185d839fbe0782a26521be69b23875
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-05-04 13:18:00 -0300

    fix accuracy check out in route response

--- routes/face-recognition.js
+++ routes/face-recognition.js
@@ -4,11 +4,9 @@ const fr = require('face-recognition');
 const router = express.Router();
 const neuralModelState = require('../models/neural.model.json');
 
-/* GET users listing. */
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
   const base64 = req.body.data.replace(/^data:image\/png;base64,/, "");
-
   let faceDescriptors = [];
   await neuralModelState.forEach((data, i) => {
     if (username === data.className) {
@@ -25,8 +23,15 @@ router.post('/', async (req, res) => {
     const image = await fr.loadImage(`uploads/buffer/${username}.jpg`);
     const face = await detector.detectFaces(image, 150);
     const predictions = recognizer.predict(face[0]);
+    const distance = predictions[0].distance;
+
     fs.unlinkSync(`uploads/buffer/${username}.jpg`)
-    res.status(200).json({ accuracy: predictions, message: 'User authorized with success' });
+
+    if (distance < 0.5) {
+      res.status(200).json({ accuracy: distance, message: 'User authorized with success' });
+    } else {
+      res.status(400).json({ accuracy: distance, message: 'User unauthorized' });
+    }
   } else {
     res.status(400).json({ error: 400, message: 'User not found' });
   }
